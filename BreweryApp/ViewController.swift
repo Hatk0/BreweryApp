d7aacf4462c2c140e490387c13eb9d2ab0768d67
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-03-29 21:20:52 +0300

    Initial Commit

---
+++ BreweryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BreweryApp
+//
+//  Created by Dmitry Yastrebov on 29.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
