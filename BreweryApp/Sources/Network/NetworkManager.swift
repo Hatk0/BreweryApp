bf0e27e6a7bef3cbf8769b66b387cf1192d1858d
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-04-01 17:06:42 +0300

    Create a method to get data from url

--- BreweryApp/Sources/Network/NetworkManager.swift
+++ BreweryApp/Sources/Network/NetworkManager.swift
@@ -22,4 +22,28 @@ final class NetworkManager {
         let request = URLRequest(url: url)
         return request
     }
+    
+    func getData(handler: @escaping (Result<Data, BreweryError>) -> Void) {
+        guard let url = createURL(), let urlRequest = createRequest(url: url) else { return }
+        
+        let session = URLSession.shared
+        session.dataTask(with: urlRequest) { data, response, error in
+            if let error = error {
+                handler(.failure(.otherError(error.localizedDescription)))
+                return
+            }
+            
+            guard let data = data, let response = response as? HTTPURLResponse else {
+                handler(.failure(.invalidResponse))
+                return
+            }
+            
+            switch response.statusCode {
+            case 200:
+                handler(.success(data))
+            default:
+                handler(.failure(.invalidStatusCode(response.statusCode)))
+            }
+        }.resume()
+    }
 }
