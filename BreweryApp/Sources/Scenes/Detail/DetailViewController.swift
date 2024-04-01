5ab71a4da3ba49c49efcd5383a0bbfff894d4ce0
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-04-01 19:34:26 +0300

    Make basic preparations for DetailViewController

---
+++ BreweryApp/Sources/Scenes/Detail/DetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DetailViewController.swift
+//  BreweryApp
+//
+//  Created by Dmitry Yastrebov on 01.04.2024.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    // MARK: - UI
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Setup
+    
+    private func setupView() {
+        
+    }
+    
+    private func setupHierarchy() {
+        
+    }
+    
+    private func setupLayout() {
+        
+    }
+}
