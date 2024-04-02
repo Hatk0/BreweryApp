af514fcfb595a4632cba2879e6ee5c68b9e8b676
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-04-02 16:37:00 +0300

    Create Labels and StackView

--- BreweryApp/Sources/Scenes/Detail/DetailViewController.swift
+++ BreweryApp/Sources/Scenes/Detail/DetailViewController.swift
@@ -15,24 +15,75 @@ class DetailViewController: UIViewController {
     
     // MARK: - UI
     
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "10 Barrel Brewing Co"
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var cityLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Bend"
+        label.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var stateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Oregon"
+        label.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var countryLabel: UILabel = {
+        let label = UILabel()
+        label.text = "United States"
+        label.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let stack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.distribution = .fill
+        stack.alignment = .center
+        stack.spacing = 10
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupView()
+        setupHierarchy()
+        setupLayout()
     }
     
     // MARK: - Setup
     
     private func setupView() {
-        
+        view.backgroundColor = .systemBackground
     }
     
     private func setupHierarchy() {
-        
+        view.addSubview(stack)
+        stack.addArrangedSubview(nameLabel)
+        stack.addArrangedSubview(cityLabel)
+        stack.addArrangedSubview(stateLabel)
+        stack.addArrangedSubview(countryLabel)
     }
     
     private func setupLayout() {
-        
+        NSLayoutConstraint.activate([
+            stack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stack.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 }
